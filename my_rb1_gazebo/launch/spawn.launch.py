ce5ab7b6adeeadd9804ac4997e97db7d15c4a68c
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-09-05 11:32:02 +0000

    Add parameter_bridge node in spawn launch file

--- my_rb1_gazebo/launch/spawn.launch.py
+++ my_rb1_gazebo/launch/spawn.launch.py
@@ -50,6 +50,23 @@ def generate_launch_description():
         output="screen",
     )
 
+    # ROS-Gazebo Bridge #
+    gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="gz_bridge",
+        arguments=[
+            "/clock" + "@rosgraph_msgs/msg/Clock" + "[gz.msgs.Clock",
+            "/cmd_vel" + "@geometry_msgs/msg/Twist" + "@gz.msgs.Twist",
+            "/tf" + "@tf2_msgs/msg/TFMessage" + "[gz.msgs.Pose_V",
+            "/odom" + "@nav_msgs/msg/Odometry" + "[gz.msgs.Odometry",
+            "/scan" + "@sensor_msgs/msg/LaserScan" + "[gz.msgs.LaserScan",
+        ],
+        remappings=[
+        ],
+        output="screen",
+    )
+
     # Create and Return the Launch Description Object #
     return LaunchDescription(
         [
@@ -60,5 +77,6 @@ def generate_launch_description():
             declare_spawn_y,
             declare_spawn_z,
             gz_spawn_entity,
+            gz_bridge,
         ]
     )
